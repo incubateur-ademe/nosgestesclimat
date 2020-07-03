a214d1209deedf5259111be86a32f31d13985954
Mael <mael.thomas@data.gouv.fr>
2020-07-03 12:32:14 +0200

    Reformation de co2.json à partir des multiples yaml

--- scripts/rulesToJSON.js
+++ scripts/rulesToJSON.js
@@ -1,9 +1,14 @@
 let yaml = require('yaml')
 let fs = require('fs')
-fs.readFile('../data/co2.yaml', 'utf8', (err, data) => {
-	if (err) console.error(err)
-	let rules = yaml.parse(data)
 
+fs.readdir('../data/', 'utf8', (err, filenames) => {
+	if (err) console.error(err)
+
+	const rules = filenames.reduce((memo, filename) => {
+		const data = fs.readFileSync('../data/' + filename, 'utf8')
+		const rules = yaml.parse(data)
+		return { ...memo, ...rules }
+	}, {})
 	fs.writeFile('./public/co2.json', JSON.stringify(rules), function (err) {
 		if (err) return console.error(err)
 		console.log('Les règles en JSON ont été écrites avec succès, bravo !')
