b0bfe570932ce730ff5d2cd43c70d95d3a164b7c
Mael <mael.thomas@data.gouv.fr>
2020-09-01 12:14:46 +0200

    :bug: Correction du script JSON->YAML

--- scripts/rulesToJSON.js
+++ scripts/rulesToJSON.js
@@ -4,11 +4,13 @@ let fs = require('fs')
 fs.readdir('./data/', 'utf8', (err, filenames) => {
 	if (err) console.error(err)
 
-	const rules = filenames.reduce((memo, filename) => {
-		const data = fs.readFileSync('./data/' + filename, 'utf8')
-		const rules = yaml.parse(data)
-		return { ...memo, ...rules }
-	}, {})
+	const rules = filenames
+		.filter((f) => f.endsWith('.yaml'))
+		.reduce((memo, filename) => {
+			const data = fs.readFileSync('./data/' + filename, 'utf8')
+			const rules = yaml.parse(data)
+			return { ...memo, ...rules }
+		}, {})
 	fs.writeFile('./public/co2.json', JSON.stringify(rules), function (err) {
 		if (err) return console.error(err)
 		console.log('Les règles en JSON ont été écrites avec succès, bravo !')
