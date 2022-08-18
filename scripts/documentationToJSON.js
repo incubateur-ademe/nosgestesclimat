e318a50820d2553dfda2d4515a901f13d3eb2a10
Mael <mael.thomas@data.gouv.fr>
2022-08-18 11:16:50 +0200

    Activation du script documentation au compiel

--- scripts/documentationToJSON.js
+++ scripts/documentationToJSON.js
@@ -2,49 +2,21 @@ let yaml = require('yaml')
 let fs = require('fs')
 let glob = require('glob')
 
-glob('documentation/actions-plus/*.md', (err2, mdFiles) => {
+glob('documentation/*/*.md', (err2, mdFiles) => {
 	const data = mdFiles.reduce((memo, filename) => {
 		const content = fs.readFileSync('./' + filename, 'utf8')
-		const dottedName = filename.replace(
-			/(documentation\/actions-plus\/|\.md)/g,
-			''
-		)
+		const dottedName = filename.replace(/(documentation\/|\.md)/g, '')
 
 		return { ...memo, [dottedName]: content }
 	}, {})
 
 	fs.writeFile(
-		'./public/actions-plus.json',
+		'./public/documentation.json',
 		JSON.stringify(data),
 		function (err) {
 			if (err) return console.error(err)
 			console.log(
-				'Les actions plus en JSON ont été écrites avec succès, bravo !'
-			)
-		}
-	)
-})
-
-glob('documentation/guide-mode-groupe/*.md', (err2, mdFiles) => {
-	const data = mdFiles.reduce((memo, filename) => {
-		const content = fs.readFileSync('./' + filename, 'utf8')
-		const guideName = filename.replace(
-			/(documentation\/guide-mode-groupe\/|\.md)/g,
-			''
-		)
-		return {
-			...memo,
-			[guideName]: content,
-		}
-	}, {})
-
-	fs.writeFile(
-		'./public/guide-mode-groupe.json',
-		JSON.stringify(data),
-		function (err) {
-			if (err) return console.error(err)
-			console.log(
-				'Les guides mode groupe en JSON ont été écrites avec succès, bravo !'
+				'Les fichiers .md de documentation/ en ont été exposées dans public/ en JSON avec succès, bravo !'
 			)
 		}
 	)
