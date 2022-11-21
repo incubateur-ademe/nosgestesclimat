d8a2e75874a51d6d9c1b047f2064df97798481f6
Clement AUGER <clem.auger@hotmail.fr>
2022-11-21 18:08:26 +0100

    ajout table répartition sectorielle

--- scripts/naf.js
+++ scripts/naf.js
@@ -1,24 +1,65 @@
 const fs = require('fs')
+const yaml = require('yaml')
 
-const nafFileName = 'data/NAF/liste_NAF.json'
+// utils
+const roundValue = (x) => Math.round(x * 100) / 100
+
+// read files
+const nafFileName = 'data/NAF/liste_NAF_level2.json'
 const sdesFileName = 'data/NAF/liste_SDES.json'
+const divisionNAFFileName = 'data/NAF/division_NAF.yaml'
+
+const readNAF = fs.readFileSync(nafFileName, 'utf8')
+const NAF_level2 = JSON.parse(readNAF)
+
+const readDivision = fs.readFileSync(divisionNAFFileName, 'utf8')
+const division_NAF = yaml.parse(readDivision)
+
+const checkDivision = (division_NAF) => {
+	Object.entries(division_NAF).map(([key, obj]) => {
+		const checkSum = Object.values(obj).reduce((acc, att) => {
+			if (att.part || att.part === 0) {
+				if (att.part === 'défaut') {
+					return true
+				} else if (isNaN(att.part)) {
+					throw new Error("L'attribut `part` doit être un nombre")
+				} else {
+					return acc + att.part
+				}
+			} else {
+				return acc
+			}
+		}, null)
+		if (!(checkSum === true || checkSum === 100)) {
+			throw new Error(
+				`La répartition des parts attribuées à chaque division doit couvrir 100% de ${key}`
+			)
+		}
+	})
+}
+
+try {
+	checkDivision(division_NAF)
+	console.log(`✅ Le fichier`, divisionNAFFileName, `a été défini correctement`)
+} catch (err) {
+	console.log(
+		`❌ Une erreur est survenue lors l'analyse du fichier`,
+		divisionNAFFileName,
+		':\n\n',
+		err.message
+	)
+	exit(-1)
+}
 
 const readSDES = fs.readFileSync(sdesFileName, 'utf8')
 const dataSDES = JSON.parse(readSDES)
 	.map((obj) => {
-		const composition = obj['code_CPA'].split('_')
-		composition.shift()
+		const CPA = obj['code_CPA'].split('CPA_')[1]
+		const composition = CPA.split('_')
 		if (composition.length === 1) {
 			return {
+				...obj,
 				code_CPA: composition[0],
-				'Émissions contenues dans les biens et services adressés à la demande finale de la France':
-					obj[
-						'Émissions contenues dans les biens et services adressés à la demande finale de la France'
-					],
-				'émissions de la production intérieure (hors exportations)':
-					obj['émissions de la production intérieure (hors exportations)'],
-				'émissions associées aux importations':
-					obj['émissions associées aux importations'],
 			}
 		} else {
 			const findNumber = /\d+/
@@ -29,44 +70,38 @@ const dataSDES = JSON.parse(readSDES)
 				{ length: indexes[1] - indexes[0] + 1 },
 				(_, i) => letter + String(indexes[0] + i).padStart(2, '0')
 			)
-			const facteur = newComposition.length
+			const compositionLength = newComposition.length
 			const newObjects = newComposition.map((elt) => {
+				const facteurPotentiel = division_NAF[CPA][elt].part
+				const facteur =
+					facteurPotentiel === 'défaut'
+						? 1 / compositionLength
+						: facteurPotentiel
 				return {
 					code_CPA: elt,
+					'Libellé CPA': NAF_level2.find((obj) => obj.code_NAF === elt)[
+						'libellé'
+					],
 					'Émissions contenues dans les biens et services adressés à la demande finale de la France':
-						obj[
-							'Émissions contenues dans les biens et services adressés à la demande finale de la France'
-						] / facteur,
+						roundValue(
+							obj[
+								'Émissions contenues dans les biens et services adressés à la demande finale de la France'
+							] * facteur
+						),
 					'émissions de la production intérieure (hors exportations)':
-						obj['émissions de la production intérieure (hors exportations)'] /
-						facteur,
-					'émissions associées aux importations':
-						obj['émissions associées aux importations'] / facteur,
+						roundValue(
+							obj['émissions de la production intérieure (hors exportations)'] *
+								facteur
+						),
+					'émissions associées aux importations': roundValue(
+						obj['émissions associées aux importations'] * facteur
+					),
 				}
 			})
 			return newObjects
 		}
 	})
 	.flat()
-fs.writeFileSync('data/NAF/liste_SDES_traitée.json', JSON.stringify(dataSDES))
-console.log(dataSDES)
-const readNAF = fs.readFileSync(nafFileName, 'utf8')
-const data = JSON.parse(readNAF).reduce((memo, elt) => {
-	const lastId = memo.length > 0 && memo[memo.length - 1].code_NAF
-	const currentId = elt.id_1 + elt.id_2
-	if (lastId !== currentId) {
-		memo.push({
-			code_NAF: currentId,
-			description: elt.label_2,
-			co2e: dataSDES.find((obj) => obj.code_CPA === currentId)
-				? dataSDES.find((obj) => obj.code_CPA === currentId)[
-						'Émissions contenues dans les biens et services adressés à la demande finale de la France'
-				  ]
-				: 0,
-		})
-	}
-	return memo
-}, [])
 
-// console.log(data)
-// fs.writeFileSync('data/NAF/liste_NAF2.json', json)
+// fs.writeFileSync('data/NAF/liste_SDES_traitée.json', JSON.stringify(dataSDES))
+// console.log(dataSDES)
