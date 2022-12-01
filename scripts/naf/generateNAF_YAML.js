2bdab7a821fd1166db2dcf0df0a866ce24142d11
Clement AUGER <clem.auger@hotmail.fr>
2022-12-01 19:41:26 +0100

    ajout titres simplifiés et description

--- scripts/naf/generateNAF_YAML.js
+++ scripts/naf/generateNAF_YAML.js
@@ -6,33 +6,56 @@ const readFile = fs.readFileSync(sdesFileName, 'utf8')
 
 const répartitionFileName = 'scripts/naf/données/répartition_NAF.yaml'
 const readFileRépartition = fs.readFileSync(répartitionFileName, 'utf8')
-
 const répartition = yaml.parse(readFileRépartition)
+
+const analyseCANAFFileName = 'scripts/naf/données/analyse_CA_naf.json'
+const readFileanalyseCANAF = fs.readFileSync(analyseCANAFFileName, 'utf8')
+const analyseNAF = JSON.parse(readFileanalyseCANAF)
+
+const titresFileName = 'scripts/naf/données/titres_raccourcis.yaml'
+const readFiletitres = fs.readFileSync(titresFileName, 'utf8')
+const titres = yaml.parse(readFiletitres)
 
 const SP_sum = []
 const SMS_sum = []
 
+const findNumber = /\d{2}/
+
 const data = JSON.parse(readFile).map(({ code_CPA, ...att }) => {
 	const ruleCPA = `naf . ${code_CPA}`
 	const ruleCPAparHab = `naf . ${code_CPA} par hab`
+	const titre = att['Libellé CPA']
+	const titre_raccourci = titres[code_CPA] || titre
+	const composition = analyseNAF[+code_CPA.match(findNumber)]?.composition || []
+	const description = composition.reduce((str, obj) => {
+		const subDescription =
+			obj.description.length > 1 &&
+			obj.description.reduce((subStr, subObj) => {
+				return subStr + `\n	- ${subObj.libellé} (${subObj.part})`
+			}, '')
+		const newStr = `\n- **${obj.libellé} (${obj.part})**`
+		const newStrWithSub = subDescription ? newStr + subDescription : newStr
+		return str + newStrWithSub
+	}, '')
 	const object = {
 		[ruleCPA]: {
-			titre: att['Libellé CPA'],
+			titre: titre_raccourci,
 			formule:
 				att[
 					'Émissions contenues dans les biens et services adressés à la demande finale de la France'
 				],
 			unité: 'ktCO2e',
+			description: `${titre}\n${description}`,
 		},
 		[ruleCPAparHab]: {
-			titre: att['Libellé CPA'],
+			titre: `${titre_raccourci} par habitant`,
 			formule: `${code_CPA} * 1000000 kgCO2e/ktCO2e / population`,
 			unité: 'kgCO2e',
+			description: `${titre} par habitant\n${description}`,
 		},
 	}
 	const répartition_SP = répartition['services publics'][code_CPA]
-	const répartition_SMS =
-		répartition['services marchands et sociétaux'][code_CPA]
+	const répartition_SMS = répartition['services marchands'][code_CPA]
 	if (répartition_SP || répartition_SMS) {
 		const objavec = {}
 		object[[ruleCPAparHab]]['avec'] = {}
@@ -40,18 +63,18 @@ const data = JSON.parse(readFile).map(({ code_CPA, ...att }) => {
 			const ruleNameSP = `naf . ${code_CPA} par hab . services publics`
 			objavec['ratio services publics'] = répartition_SP
 			object[ruleNameSP] = {
-				titre: `attribution SP ${att['Libellé CPA']}`,
+				titre: `${répartition_SP} ${titre_raccourci}`,
 				formule: `${code_CPA} par hab * ratio services publics`,
 				unité: 'kgCO2e',
 			}
 			SP_sum.push(ruleNameSP)
 		}
 		if (répartition_SMS) {
-			const ruleNameSMS = `naf . ${code_CPA} par hab . services marchands et sociétaux`
-			objavec['ratio services marchands et sociétaux'] = répartition_SMS
+			const ruleNameSMS = `naf . ${code_CPA} par hab . services marchands`
+			objavec['ratio services marchands'] = répartition_SMS
 			object[ruleNameSMS] = {
-				titre: `attribution SMS ${att['Libellé CPA']}`,
-				formule: `${code_CPA} par hab * ratio services marchands et sociétaux`,
+				titre: `${répartition_SMS} ${titre_raccourci}`,
+				formule: `${code_CPA} par hab * ratio services marchands`,
 				unité: 'kgCO2e',
 			}
 			SMS_sum.push(ruleNameSMS)
@@ -64,10 +87,10 @@ const data = JSON.parse(readFile).map(({ code_CPA, ...att }) => {
 const dataObject = Object.assign({}, ...data)
 
 const SPobject = {
-	publics: {
+	'services publics': {
 		titre: 'Services publics',
 		couleur: '#0c2461',
-		abréviation: 'Public',
+		abréviation: 'Publics',
 		icônes: '🏛',
 		formule: { somme: SP_sum },
 		unité: 'ktCO2e',
@@ -75,17 +98,23 @@ const SPobject = {
 }
 
 const SMSobject = {
-	'marchands et sociétaux': {
-		titre: 'Services marchands et sociétaux',
+	'services marchands': {
+		titre: 'Services marchands',
 		couleur: '#3c0c61',
-		abréviation: 'Marchand',
+		abréviation: 'Marchands',
 		icônes: '✉️',
 		formule: { somme: SMS_sum },
 		unité: 'ktCO2e',
 	},
 }
 
-// console.log(yaml.stringify(dataObject))
+console.log(yaml.stringify(dataObject))
 fs.writeFileSync('data/naf/naf.yaml', yaml.stringify(dataObject))
-fs.writeFileSync('data/services publics/SP.yaml', yaml.stringify(SPobject))
-fs.writeFileSync('data/services publics/SMS.yaml', yaml.stringify(SMSobject))
+fs.writeFileSync(
+	'data/services sociétaux/services publics.yaml',
+	yaml.stringify(SPobject)
+)
+fs.writeFileSync(
+	'data/services sociétaux/services marchands.yaml',
+	yaml.stringify(SMSobject)
+)
