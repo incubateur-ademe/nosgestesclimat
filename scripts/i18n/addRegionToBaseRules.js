4d213ba582092d453ea1875504a0c1ba933a2fca
Clement AUGER <clem.auger@hotmail.fr>
2023-01-30 20:01:01 +0100

    no cli in addRegionToBaseRules

--- scripts/i18n/addRegionToBaseRules.js
+++ scripts/i18n/addRegionToBaseRules.js
@@ -5,23 +5,16 @@
 	NOTE: this function is used by the RulesProvider.js file of the website.
 */
 
-const cli = require('./cli.js')
-
 const R = require('ramda')
 
 const addRegionToBaseRules = (baseRules, translatedRules) => {
 	const updateBaseRules = (key, val) => {
 		if (R.path(key, baseRules)) {
 			baseRules = R.assocPath(key, val, baseRules)
-		} else {
-			cli.printWarn(
-				`[SKIPPED] - There is no "${key[1]}" attibute in the rule "${key[0]}" of base rules.`
-			)
 		}
 	}
 	Object.entries(translatedRules).forEach(([rule, attrs]) => {
 		if (!Object.keys(baseRules).includes(rule)) {
-			cli.printWarn(`[SKIPPED] - There is no rule "${rule}" in base rules.`)
 			return baseRules
 		}
 		Object.entries(attrs).forEach(([attr, transVal]) => {
