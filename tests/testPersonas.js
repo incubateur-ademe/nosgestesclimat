8062bd848214aa852585660b7e7e383ea77e1b9d
Emile Rolley <emile.rolley@tuta.io>
2023-02-27 15:45:32 +0100

    refactor(test/personas): add a CLI mode with diff in percent

--- tests/testPersonas.js
+++ tests/testPersonas.js
@@ -1,6 +1,93 @@
 const fs = require('fs/promises')
+const yargs = require('yargs')
+const i18nUtils = require('./../scripts/i18n/utils')
+const { disabledLogger } = require('publiopti')
+
+function col(str, color) {
+	return color + str + '\x1b[0m'
+}
+
+const c = {
+	orange: (str) => col(str, '\x1b[33m'),
+	red: (str) => col(str, '\x1b[31m'),
+	green: (str) => col(str, '\x1b[32m'),
+	white: (str) => col(str, '\x1b[37m'),
+	gray: (str) => col(str, '\x1b[90m'),
+	yellow: (str) => col(str, '\x1b[93m'),
+	dim: (str) => col(str, '\x1b[2m'),
+}
 
 const Engine = require('publicodes').default
+
+const { country, language, markdown } = yargs(process.argv.slice(2))
+	.usage('Compare local and prod personas results\n\nUsage: $0 [options]')
+	.option('country', {
+		alias: 'c',
+		describe: 'Target country code',
+		type: 'string',
+		default: 'FR',
+	})
+	.option('language', {
+		alias: 'l',
+		describe: 'Target language code',
+		type: 'string',
+		default: i18nUtils.defaultLang,
+		choices: i18nUtils.availableLanguages,
+	})
+	.option('markdown', {
+		alias: 'm',
+		type: 'boolean',
+		description: 'Prints the result in a Markdown table format.',
+	})
+
+	.help('h')
+	.alias('h', 'help').argv
+
+const kgCO2Str = c.dim('(kg CO2e)')
+
+function formatValueInKgCO2e(value) {
+	return c.yellow(Math.round(value).toLocaleString('fr')) + ' ' + kgCO2Str
+}
+
+function fmtCLIErr(localResult, prodResult, diff, diffPercent, name, color) {
+	const sign = diff > 0 ? '+' : ''
+	const hd = color(diffPercent < 1 ? '[WARN]' : '[FAIL]')
+	return `${hd} ${name} [${color(sign + diff)} ${kgCO2Str}, ${color(
+		sign + diffPercent
+	)}%]:\n${hd}\t${formatValueInKgCO2e(localResult)} != ${formatValueInKgCO2e(
+		prodResult
+	)}`
+}
+
+function printResults(localResults, prodResults) {
+	if (markdown) {
+		console.log('| Persona | Total PR (kg CO2e) | Total Prod (kg CO2e) |')
+		console.log('|:-----|:------:|:------:|:------:|')
+	}
+	for (let name in localResults) {
+		if (localResults[name] !== prodResults[name]) {
+			const localResult = Math.round(localResults[name])
+			const prodResult = Math.round(prodResults[name])
+			const diff = localResult - prodResult
+			const diffPercent = Math.round((diff / prodResult) * 100)
+			const color = diffPercent <= 1 ? c.orange : c.red
+
+			console.log(
+				markdown
+					? `|${name}|${Math.round(localResults[name])}|${Math.round(
+							prodResults[name]
+					  )}|`
+					: fmtCLIErr(localResult, prodResult, diff, diffPercent, name, color)
+			)
+		} else if (!markdown) {
+			console.log(
+				`${c.green('[PASS]')} ${name}: ${formatValueInKgCO2e(
+					localResults[name]
+				)}`
+			)
+		}
+	}
+}
 
 function comparePersonas(country, language) {
 	const localRules = fs
@@ -8,18 +95,49 @@ function comparePersonas(country, language) {
 			encoding: 'utf8',
 		})
 		.then((res) => JSON.parse(res))
+		.catch((e) => {
+			console.log(
+				`No local rules found for ${country} and ${language}, using prod rules:`
+			)
+			console.log(e.message)
+			process.exit(-1)
+		})
+
 	const localPersonas = fs
 		.readFile(`./public/personas-${language}.json`, {
 			encoding: 'utf8',
 		})
 		.then((res) => JSON.parse(res))
+		.catch((e) => {
+			console.log(
+				`No local personas found for ${country} and ${language}, using prod personas:`
+			)
+			console.log(e.message)
+			process.exit(-1)
+		})
 
 	const prodRules = fetch(
 		`https://data.nosgestesclimat.fr/co2-model.${country}-lang.${language}.json`
-	).then((res) => res.json())
+	)
+		.then((res) => res.json())
+		.catch((e) => {
+			console.log(
+				`No prod rules found for ${country} and ${language}, using local rules:`
+			)
+			console.log(e.message)
+			process.exit(-1)
+		})
 	const prodPersonas = fetch(
 		`https://data.nosgestesclimat.fr/personas-${language}.json`
-	).then((res) => res.json())
+	)
+		.then((res) => res.json())
+		.catch((e) => {
+			console.log(
+				`No prod personas found for ${country} and ${language}, using local personas:`
+			)
+			console.log(e.message)
+			process.exit(-1)
+		})
 
 	const allData = Promise.all([
 		localRules,
@@ -31,12 +149,12 @@ function comparePersonas(country, language) {
 	allData.then((res) => {
 		const localResults = testPersonas(res[0], res[1])
 		const prodResults = testPersonas(res[2], res[3])
-		writeResults(localResults, prodResults)
+		printResults(localResults, prodResults)
 	})
 }
 
 function testPersonas(rules, personas) {
-	const engine = new Engine(rules)
+	const engine = new Engine(rules, { logger: disabledLogger })
 	const missingVariables = Object.keys(
 		engine.evaluate('bilan').missingVariables
 	)
@@ -45,8 +163,8 @@ function testPersonas(rules, personas) {
 
 	const results = {}
 
-	for (persona of personasRules) {
-		const personaData = persona.data.situation || persona.data
+	for (let persona of personasRules) {
+		const personaData = persona.data.situation ?? persona.data
 		const validPersonaRules = Object.keys(personaData).filter((rule) =>
 			missingVariables.includes(rule)
 		)
@@ -62,18 +180,5 @@ function testPersonas(rules, personas) {
 	return results
 }
 
-function writeResults(localResults, prodResults) {
-	console.log('| Nom | Total (PR) | Total (Prod) |')
-	console.log('|:-----|:------:|:------:|')
-	for (let name in localResults) {
-		if (localResults[name] !== prodResults[name]) {
-			console.log(
-				`|${name}|${Math.round(localResults[name])} kg CO2e|${Math.round(
-					prodResults[name]
-				)} kg CO2e|`
-			)
-		}
-	}
-}
-
-comparePersonas('FR', 'fr')
+// TODO: compares all contry codes + optim
+comparePersonas(country, language)
