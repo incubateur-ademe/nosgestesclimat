278cb515b38d7f4b40df42189f8c295e41745a63
Emile Rolley <emile.rolley@tuta.io>
2023-03-30 15:31:49 +0200

    fix(script/i18n): use the the getModelFromSource in check-translation.js

--- scripts/i18n/check-translation.js
+++ scripts/i18n/check-translation.js
@@ -11,6 +11,7 @@ const { exit } = require('process')
 
 const cli = require('./cli')
 const utils = require('./utils')
+const { getModelFromSource } = require('../getModelFromSource')
 
 const { destLangs, srcFile, markdown } = cli.getArgs(
 	`Checks all rules have been translated.`,
@@ -52,53 +53,43 @@ function manageNotUpToDateRuleTranslations(
 	}
 }
 
-glob(`${srcFile}`, { ignore: ['data/i18n/**'] }, (_, files) => {
-	const rules = R.mergeAll(
-		files.reduce((acc, filename) => {
-			try {
-				return acc.concat(utils.readYAML(filename))
-			} catch (err) {
-				cli.printErr('An error occured while reading the file ' + filename + '')
-				cli.printErr(err)
-				exit(-1)
-			}
-		}, [])
+const rules = getModelFromSource(srcFile, ['data/i18n/**'], {
+	verbose: !markdown,
+})
+
+cli.printChecksResultTableHeader(markdown)
+
+destLangs.forEach((destLang) => {
+	const destPath = `data/i18n/t9n/translated-rules-${destLang}.yaml`
+	const destRules = R.mergeAll(utils.readYAML(path.resolve(destPath)))
+	const missingRules = utils.getMissingRules(rules, destRules)
+	const missingRuleNames = missingRules.map(
+		(obj) => `${obj.rule} -> ${obj.attr}`
 	)
+	const nbMissing = missingRules.length
 
-	cli.printChecksResultTableHeader(markdown)
-
-	destLangs.forEach((destLang) => {
-		const destPath = `data/i18n/t9n/translated-rules-${destLang}.yaml`
-		const destRules = R.mergeAll(utils.readYAML(path.resolve(destPath)))
-		const missingRules = utils.getMissingRules(rules, destRules)
-		const missingRuleNames = missingRules.map(
-			(obj) => `${obj.rule} -> ${obj.attr}`
+	cli.printChecksResult(
+		nbMissing,
+		missingRuleNames,
+		'rules',
+		destLang,
+		markdown
+	)
+	if (nbMissing > 0 && cli.promptYesNo(`Do you want to log missing rules?`)) {
+		missingRules.map(({ rule: ruleName, attr }) =>
+			console.log(cli.styledRuleNameWithOptionalAttr(ruleName, attr))
 		)
-		const nbMissing = missingRules.length
+	}
 
-		cli.printChecksResult(
-			nbMissing,
-			missingRuleNames,
-			'rules',
-			destLang,
-			markdown
-		)
-		if (nbMissing > 0 && cli.promptYesNo(`Do you want to log missing rules?`)) {
-			missingRules.map(({ rule: ruleName, attr }) =>
-				console.log(cli.styledRuleNameWithOptionalAttr(ruleName, attr))
-			)
-		}
-
-		const notUpToDateRuleTranslations = utils.getNotUpToDateRuleTranslations(
-			rules,
+	const notUpToDateRuleTranslations = utils.getNotUpToDateRuleTranslations(
+		rules,
+		destRules
+	)
+	if (!markdown && notUpToDateRuleTranslations.length > 0) {
+		manageNotUpToDateRuleTranslations(
+			notUpToDateRuleTranslations,
+			destPath,
 			destRules
 		)
-		if (!markdown && notUpToDateRuleTranslations.length > 0) {
-			manageNotUpToDateRuleTranslations(
-				notUpToDateRuleTranslations,
-				destPath,
-				destRules
-			)
-		}
-	})
+	}
 })
