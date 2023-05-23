5db96a5f9f7bb3931e8c34405ca17e69d0ce32c7
David Boureau <bdavidxyz@gmail.com>
2023-05-23 21:05:25 +0200

    refactor: better indent

--- tests/livraison/emballage.test.js
+++ tests/livraison/emballage.test.js
@@ -4,28 +4,25 @@ import { parse } from 'yaml'
 
 test("livraison colis . emballage : poids emballage * empreinte carton", () => {
     
+    // Given
     const rules = `
-poids emballage: 4 kg
-empreinte carton: 4 CO2e
-livraison colis . emballage:
-  titre: Emballage du colis
-  note: Hypothèse d'un emballage carton uniquement
-  formule: poids emballage * empreinte carton
+        poids emballage: 4 kg
+        empreinte carton: 4 CO2e
+        livraison colis . emballage:
+            titre: Emballage du colis
+            note: Hypothèse d'un emballage carton uniquement
+            formule: poids emballage * empreinte carton
     `
-
-    // // publicodes ne prend plus en entrée du YAML, vous devez parser vous-même votre code source
     const parsedRules = parse(rules)
-
-    // // On initialise un moteur en lui donnant le publicodes sous forme d'objet javascript.
-    // // Ce publicodes va être parsé
     const engine = new Engine(parsedRules)
 
+    // When
     engine.setSituation({
         "poids emballage": '80',
         "empreinte carton": '2',
     })
     const evaluated = engine.evaluate('livraison colis . emballage')
-    // // teste la valeur brute
+    // Then
     expect(evaluated.nodeValue).toEqual(160)
 })  
 
