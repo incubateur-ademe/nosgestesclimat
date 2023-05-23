e2234165c1c198707f99138341e809e629c7ec26
David Boureau <bdavidxyz@gmail.com>
2023-05-23 22:00:21 +0200

    refactor: cleaned test

--- tests/livraison/emballage.test.js
+++ tests/livraison/emballage.test.js
@@ -3,52 +3,22 @@ import { parse } from 'yaml'
 import { extractRule, yamlToJson } from './utils'
 const fs = require('fs');
 
-test("Importé. livraison colis . emballage : poids emballage * empreinte carton", () => {
+test("livraison colis . emballage : poids emballage * empreinte carton", () => {
 
     // Given
-    const rules = yamlToJson('data/livraison/emballage.yaml');
-    console.log('rules', rules);
-
+    const jsonRules = yamlToJson('data/livraison/emballage.yaml');
+    const testedRule = extractRule('livraison colis . emballage', jsonRules)
     const inputs = {
         'poids emballage': 80,
         'empreinte carton': 2,
     }
 
-    const testedRule = extractRule('livraison colis . emballage', rules)
-    console.log('testedRule', testedRule);
-
-    const actualRules = { ...inputs, ...testedRule }
-    console.log('actualRules', actualRules);
-    const engine = new Engine(actualRules)
-
     // When
+    const rule = { ...inputs, ...testedRule }
+    const engine = new Engine(rule)
     const evaluated = engine.evaluate('livraison colis . emballage')
+
     // Then
     expect(evaluated.nodeValue).toEqual(160)
 })
-
-test("En dur. livraison colis . emballage : poids emballage * empreinte carton", () => {
-
-    // Given
-    const rules = `
-        poids emballage: 4 kg
-        empreinte carton: 4 CO2e
-        livraison colis . emballage:
-            titre: Emballage du colis
-            note: Hypothèse d'un emballage carton uniquement
-            formule: poids emballage * empreinte carton
-    `
-    const parsedRules = parse(rules)
-    console.log('parsedRules2', parsedRules);
-    const engine = new Engine(parsedRules)
-
-    // When
-    engine.setSituation({
-        "poids emballage": '80',
-        "empreinte carton": '2',
-    })
-    const evaluated = engine.evaluate('livraison colis . emballage')
-    // Then
-    expect(evaluated.nodeValue).toEqual(160)
-})  
 
