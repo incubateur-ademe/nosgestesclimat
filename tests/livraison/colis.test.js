961b6972c37071b0c9e98569dabc9e0426cd6dfd
David Boureau <bdavidxyz@gmail.com>
2023-06-09 10:10:50 +0200

    test: livraison colis . informations . volume ok

--- tests/livraison/colis.test.js
+++ tests/livraison/colis.test.js
@@ -49,3 +49,61 @@ testColis(
   },
   36
 )
+
+testColis(
+  'livraison colis . scénario . magasin traditionnel',
+  "Pour une livraison en magasin traditionnel, l'empreinte est la somme de : entrepot stockage, transport inter plateformes, plateformes, magasin, déplacement consommateur",
+  {   
+    "entrepot stockage": 1,
+    "transport inter plateformes": 2,
+    "plateformes": 3,
+    "magasin": 4,
+    "déplacement consommateur": 5,
+  },
+  15
+)
+
+testColis(
+  'livraison colis . informations . volume',
+  "Volume du colis : pour la grand consommation, le volume d'un colis est de 45000 cm3",
+  {   
+    "catégorie": "'grande consommation'",
+  },
+  45000
+)
+
+testColis(
+  'livraison colis . informations . volume',
+  "Volume du colis : pour l'habillement, le volume d'un colis est de 10000 cm3",
+  {   
+    "catégorie": "'habillement'",
+  },
+  10000
+)
+
+testColis(
+  'livraison colis . informations . volume',
+  "Volume du colis : pour un bien culturel, le volume d'un colis est de 750 cm3",
+  {   
+    "catégorie": "'culturel'",
+  },
+  750
+)
+
+testColis(
+  'livraison colis . informations . volume',
+  "Volume du colis : pour un équipements volumineux, le volume d'un colis est de 300000 cm3",
+  {   
+    "catégorie": "'équipements volumineux'",
+  },
+  300000
+)
+
+testColis(
+  'livraison colis . informations . volume',
+  "Volume du colis : pour tout autre produit, le volume considéré est de 90000 cm3",
+  {   
+    "catégorie": "'autre'",
+  },
+  90000
+)
