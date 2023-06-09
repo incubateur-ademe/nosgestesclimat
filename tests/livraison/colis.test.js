975ae965f5f84be12348a977e6c43a927148c8d6
Clement AUGER <clem.auger@hotmail.fr>
2023-06-09 16:09:49 +0200

    tentative test scénario synthèse

--- tests/livraison/colis.test.js
+++ tests/livraison/colis.test.js
@@ -1,163 +1,176 @@
 import { testOf } from './utils'
 
-const testColis= (publiProp, descr, inputs, output) => {
-  testOf('data/livraison/livraison colis.yaml', publiProp, descr, inputs, output)
+const testColis = (publiProp, descr, inputs, output) => {
+	testOf(
+		'data/livraison/livraison colis.yaml',
+		publiProp,
+		descr,
+		inputs,
+		output
+	)
 }
 
 testColis(
-  'livraison colis . scénario . domicile',
-  "Pour une livraison à domicile, l'empreinte est donnée par la somme de : commande en ligne, emballage, entrepot stockage, transport inter plateformes, plateformes, transport livraison",
-  {   
-    "commande en ligne": 1,
-    "emballage": 2,
-    "entrepot stockage": 3,
-    "transport inter plateformes": 4,
-    "plateformes": 5,
-    "transport livraison": 6
-  },
-  21
+	'livraison colis . scénario . scénario 1 synthèse',
+	'Le résultat du scénario 1 de la synthèse doit être compris entre 540 et 542 gCO2e',
+	{},
+	[540, 542]
 )
 
 testColis(
-  'livraison colis . scénario . point de retrait',
-  "Pour une livraison en point de retrait, l'empreinte est la même qu'en livraison à domicile, mais en plus, on rajoute le point de retrait et le déplacement consommateur",
-  {   
-    "commande en ligne": 1,
-    "emballage": 2,
-    "entrepot stockage": 3,
-    "transport inter plateformes": 4,
-    "plateformes": 5,
-    "transport livraison": 6,
-    "point de retrait": 7,
-    "déplacement consommateur": 8,
-  },
-  36
+	'livraison colis . scénario . domicile',
+	"Pour une livraison à domicile, l'empreinte est donnée par la somme de : commande en ligne, emballage, entrepot stockage, transport inter plateformes, plateformes, transport livraison",
+	{
+		'commande en ligne': 1,
+		emballage: 2,
+		'entrepot stockage': 3,
+		'transport inter plateformes': 4,
+		plateformes: 5,
+		'transport livraison': 6,
+	},
+	21
 )
 
 testColis(
-  'livraison colis . scénario . click and collect',
-  "Pour une livraison en click and collect, l'empreinte est la même qu'en livraison à domicile, mais en plus, on rajoute le magasin et le déplacement consommateur",
-  {   
-    "commande en ligne": 1,
-    "emballage": 2,
-    "entrepot stockage": 3,
-    "transport inter plateformes": 4,
-    "plateformes": 5,
-    "transport livraison": 6,
-    "magasin": 7,
-    "déplacement consommateur": 8,
-  },
-  36
+	'livraison colis . scénario . point de retrait',
+	"Pour une livraison en point de retrait, l'empreinte est la même qu'en livraison à domicile, mais en plus, on rajoute le point de retrait et le déplacement consommateur",
+	{
+		'commande en ligne': 1,
+		emballage: 2,
+		'entrepot stockage': 3,
+		'transport inter plateformes': 4,
+		plateformes: 5,
+		'transport livraison': 6,
+		'point de retrait': 7,
+		'déplacement consommateur': 8,
+	},
+	36
 )
 
 testColis(
-  'livraison colis . scénario . magasin traditionnel',
-  "Pour une livraison en magasin traditionnel, l'empreinte est la somme de : entrepot stockage, transport inter plateformes, plateformes, magasin, déplacement consommateur",
-  {   
-    "entrepot stockage": 1,
-    "transport inter plateformes": 2,
-    "plateformes": 3,
-    "magasin": 4,
-    "déplacement consommateur": 5,
-  },
-  15
+	'livraison colis . scénario . click and collect',
+	"Pour une livraison en click and collect, l'empreinte est la même qu'en livraison à domicile, mais en plus, on rajoute le magasin et le déplacement consommateur",
+	{
+		'commande en ligne': 1,
+		emballage: 2,
+		'entrepot stockage': 3,
+		'transport inter plateformes': 4,
+		plateformes: 5,
+		'transport livraison': 6,
+		magasin: 7,
+		'déplacement consommateur': 8,
+	},
+	36
 )
 
 testColis(
-  'livraison colis . informations . volume',
-  "Volume du colis : pour la grand consommation, le volume d'un colis est de 45000 cm3",
-  {   
-    "catégorie": "'grande consommation'",
-  },
-  45000
+	'livraison colis . scénario . magasin traditionnel',
+	"Pour une livraison en magasin traditionnel, l'empreinte est la somme de : entrepot stockage, transport inter plateformes, plateformes, magasin, déplacement consommateur",
+	{
+		'entrepot stockage': 1,
+		'transport inter plateformes': 2,
+		plateformes: 3,
+		magasin: 4,
+		'déplacement consommateur': 5,
+	},
+	15
 )
 
 testColis(
-  'livraison colis . informations . volume',
-  "Volume du colis : pour l'habillement, le volume d'un colis est de 10000 cm3",
-  {   
-    "catégorie": "'habillement'",
-  },
-  10000
+	'livraison colis . informations . volume',
+	"Volume du colis : pour la grand consommation, le volume d'un colis est de 45000 cm3",
+	{
+		catégorie: "'grande consommation'",
+	},
+	45000
 )
 
 testColis(
-  'livraison colis . informations . volume',
-  "Volume du colis : pour un bien culturel, le volume d'un colis est de 750 cm3",
-  {   
-    "catégorie": "'culturel'",
-  },
-  750
+	'livraison colis . informations . volume',
+	"Volume du colis : pour l'habillement, le volume d'un colis est de 10000 cm3",
+	{
+		catégorie: "'habillement'",
+	},
+	10000
 )
 
 testColis(
-  'livraison colis . informations . volume',
-  "Volume du colis : pour un équipements volumineux, le volume d'un colis est de 300000 cm3",
-  {   
-    "catégorie": "'équipements volumineux'",
-  },
-  300000
+	'livraison colis . informations . volume',
+	"Volume du colis : pour un bien culturel, le volume d'un colis est de 750 cm3",
+	{
+		catégorie: "'culturel'",
+	},
+	750
 )
 
 testColis(
-  'livraison colis . informations . volume',
-  "Volume du colis : pour tout autre produit, le volume considéré est de 90000 cm3",
-  {   
-    "catégorie": "'autre'",
-  },
-  90000
+	'livraison colis . informations . volume',
+	"Volume du colis : pour un équipements volumineux, le volume d'un colis est de 300000 cm3",
+	{
+		catégorie: "'équipements volumineux'",
+	},
+	300000
 )
 
 testColis(
-  'livraison colis . informations . volume',
-  "Volume du colis : pour tout autre produit, le volume considéré est de 90000 cm3",
-  {   
-    "catégorie": "'autre'",
-  },
-  90000
+	'livraison colis . informations . volume',
+	'Volume du colis : pour tout autre produit, le volume considéré est de 90000 cm3',
+	{
+		catégorie: "'autre'",
+	},
+	90000
 )
 
 testColis(
-  'livraison colis . informations . poids',
-  "Poids du colis : grande consommation, 20kg",
-  {   
-    "catégorie": "'grande consommation'",
-  },
-  20
+	'livraison colis . informations . volume',
+	'Volume du colis : pour tout autre produit, le volume considéré est de 90000 cm3',
+	{
+		catégorie: "'autre'",
+	},
+	90000
 )
 
 testColis(
-  'livraison colis . informations . poids',
-  "Poids du colis : habillement, 1.5kg",
-  {   
-    "catégorie": "'habillement'",
-  },
-  1.5
+	'livraison colis . informations . poids',
+	'Poids du colis : grande consommation, 20kg',
+	{
+		catégorie: "'grande consommation'",
+	},
+	20
 )
 
 testColis(
-  'livraison colis . informations . poids',
-  "Poids du colis : culturel, 200g",
-  {   
-    "catégorie": "'culturel'",
-  },
-  0.2
+	'livraison colis . informations . poids',
+	'Poids du colis : habillement, 1.5kg',
+	{
+		catégorie: "'habillement'",
+	},
+	1.5
 )
 
 testColis(
-  'livraison colis . informations . poids',
-  "Poids du colis : équipements volumineux, 70kg",
-  {   
-    "catégorie": "'équipements volumineux'",
-  },
-  70
+	'livraison colis . informations . poids',
+	'Poids du colis : culturel, 200g',
+	{
+		catégorie: "'culturel'",
+	},
+	0.2
 )
 
 testColis(
-  'livraison colis . informations . poids',
-  "Poids du colis : pour tout autre produit, le poids considéré est de 18kg",
-  {   
-    "catégorie": "'autre'",
-  },
-  18
+	'livraison colis . informations . poids',
+	'Poids du colis : équipements volumineux, 70kg',
+	{
+		catégorie: "'équipements volumineux'",
+	},
+	70
+)
+
+testColis(
+	'livraison colis . informations . poids',
+	'Poids du colis : pour tout autre produit, le poids considéré est de 18kg',
+	{
+		catégorie: "'autre'",
+	},
+	18
 )
--- tests/livraison/utils.js
+++ tests/livraison/utils.js
@@ -1,40 +1,39 @@
-const fs = require('fs');
+const fs = require('fs')
 import { parse } from 'yaml'
 import Engine from 'publicodes'
 
 // Select properties inside an object.
 // See https://dev.to/nas5w/how-to-select-or-omit-properties-from-an-object-in-javascript-3ina
 function pick(obj, ...props) {
-  return props.reduce(function (result, prop) {
-    result[prop] = obj[prop];
-    return result;
-  }, {});
+	return props.reduce(function (result, prop) {
+		result[prop] = obj[prop]
+		return result
+	}, {})
 }
 
 function yamlToJson(filePath) {
-  return parse(fs.readFileSync(filePath, 'utf8'));
+	return parse(fs.readFileSync(filePath, 'utf8'))
 }
 
 const testOf = (yaml, publiProp, descr, inputs, output) => {
-  test(`${publiProp} ${descr}`, () => {
+	test(`${publiProp} ${descr}`, () => {
+		// Given
+		const jsonRules = yamlToJson(yaml)
+		const testedRule = pick(jsonRules, publiProp)
 
-    // Given
-    const jsonRules = yamlToJson(yaml);
-    const testedRule = pick(jsonRules, publiProp)
+		// When
+		const rule = { ...inputs, ...testedRule }
+		const engine = new Engine(rule)
+		const evaluated = engine.evaluate(publiProp)
 
-    // When
-    const rule = { ...inputs, ...testedRule }
-    const engine = new Engine(rule)
-    const evaluated = engine.evaluate(publiProp)
-
-    // Then
-    expect(evaluated.nodeValue).toEqual(output)
-
-  })
+		// Then
+		if (Array.isArray(output)) {
+			expect(evaluated.nodeValue).toBeGreaterThanOrEqual(output[0]) &&
+				expect(evaluated.nodeValue).toBeLessThanOrEqual(output[1])
+		} else {
+			expect(evaluated.nodeValue).toEqual(output)
+		}
+	})
 }
 
-export {
-  pick,
-  yamlToJson,
-  testOf,
-}
+export { pick, yamlToJson, testOf }
