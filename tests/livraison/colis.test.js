7dfa1f43d77a18cb4d4609cbbc228bdb29827cea
David Boureau <bdavidxyz@gmail.com>
2023-06-09 09:58:52 +0200

    feat: livraison colis . scénario . click and collect

--- tests/livraison/colis.test.js
+++ tests/livraison/colis.test.js
@@ -17,3 +17,35 @@ testColis(
   },
   21
 )
+
+testColis(
+  'livraison colis . scénario . point de retrait',
+  "Pour une livraison en point de retrait, l'empreinte est la même qu'en livraison à domicile, mais en plus, on rajoute le point de retrait et le déplacement consommateur",
+  {   
+    "commande en ligne": 1,
+    "emballage": 2,
+    "entrepot stockage": 3,
+    "transport inter plateformes": 4,
+    "plateformes": 5,
+    "transport livraison": 6,
+    "point de retrait": 7,
+    "déplacement consommateur": 8,
+  },
+  36
+)
+
+testColis(
+  'livraison colis . scénario . click and collect',
+  "Pour une livraison en click and collect, l'empreinte est la même qu'en livraison à domicile, mais en plus, on rajoute le magasin et le déplacement consommateur",
+  {   
+    "commande en ligne": 1,
+    "emballage": 2,
+    "entrepot stockage": 3,
+    "transport inter plateformes": 4,
+    "plateformes": 5,
+    "transport livraison": 6,
+    "magasin": 7,
+    "déplacement consommateur": 8,
+  },
+  36
+)
