63fda1670b01922d38dea724f400aef3b2e2bff5
Emile Rolley <emile.rolley@tuta.io>
2023-12-12 11:52:43 +0100

    feat(tests): add version argument to test personas against nightly and latest

--- tests/testPersonas.mjs
+++ tests/testPersonas.mjs
@@ -4,22 +4,22 @@ import {
   getArgs,
   getLocalRules,
   getLocalPersonas,
-  getProdRules,
-  getProdPersonas
+  getRulesFromAPI,
+  getPersonasFromAPI
 } from './commons.mjs'
 
-const { country, language, markdown } = getArgs()
+const { country, language, markdown, version } = getArgs()
 
 const localRules = getLocalRules(country, language)
 const localPersonas = getLocalPersonas(country, language)
 
-const prodRules = getProdRules(country, language)
-const prodPersonas = getProdPersonas(country, language)
+const prodRules = getRulesFromAPI(version, country, language)
+const prodPersonas = getPersonasFromAPI(version, country, language)
 
 Promise.all([localRules, localPersonas, prodRules, prodPersonas]).then(
   (res) => {
     const localResults = testPersonas(res[0], res[1])
     const prodResults = testPersonas(res[2], res[3])
-    printResults(localResults, prodResults, markdown)
+    printResults(localResults, prodResults, markdown, version)
   }
 )
