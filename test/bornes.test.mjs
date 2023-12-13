41777430886b455192a186134aac4790a012006d
Clément <clem.auger@hotmail.fr>
2023-12-13 18:18:51 +0100

    feat: tentative test

---
+++ test/bornes.test.mjs
@@ -0,0 +1,21 @@
+import { testOf, sum } from './utils.mjs'
+
+const testBornes = (evaluatedDottedName, description, situation, result) => {
+  testOf(
+    'data/**/*.publicodes',
+    evaluatedDottedName,
+    description,
+    situation,
+    result
+  )
+}
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3)
+})
+
+testBornes(
+  'transport . voiture . voyageurs',
+  "Lorsque l'utilisateur entre 0 voyageurs, le nombre de voyageurs vaut 1.",
+  { 'transport . voiture . saisie voyageurs': 0 }
+)
---
+++ test/utils.mjs
@@ -0,0 +1,24 @@
+import Engine from 'publicodes'
+import { getModelFromSource } from '@publicodes/tools/compilation'
+
+function testOf(srcFile, evaluatedDottedName, description, situation, output) {
+  test(`${evaluatedDottedName} ${description}`, () => {
+    // Given
+    const rules = getModelFromSource(srcFile, {
+      ignore: ['data/i18n/**']
+    })
+    // When
+    const engine = new Engine(rules)
+    engine.setSituation(situation)
+    const evaluated = engine.evaluate(evaluatedDottedName)
+
+    // Then
+    expect(evaluated.nodeValue).toEqual(output)
+  })
+}
+
+function sum(a, b) {
+  return a + b
+}
+
+export { testOf, sum }
