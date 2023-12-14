8e28b72c830a13734c3d333e152511d648bc05e6
Clément <clem.auger@hotmail.fr>
2023-12-14 16:52:16 +0100

    feat: ajout plancher pour `habitants` et `voyageurs`

--- tests/unit/utils.mjs
+++ tests/unit/utils.mjs
@@ -1,25 +1,30 @@
 import Engine from 'publicodes'
 import { getModelFromSource } from '@publicodes/tools/compilation'
-import { expect, test } from 'bun:test'
+import { expect, test, describe } from 'bun:test'
 
-function testOf(srcFile, evaluatedDottedName, description, situation, output) {
-  test(`${evaluatedDottedName} ${description}`, () => {
-    // Given
-    const rules = getModelFromSource(srcFile, {
-      ignore: ['data/i18n/**']
+function testOf(
+  srcFile,
+  macroDescription,
+  evaluatedDottedName,
+  description,
+  situation,
+  output
+) {
+  describe(macroDescription, () => {
+    test(`${evaluatedDottedName} ${description}`, () => {
+      // Given
+      const rules = getModelFromSource(srcFile, {
+        ignore: ['data/i18n/**']
+      })
+      // When
+      const engine = new Engine(rules)
+      engine.setSituation(situation)
+      const evaluated = engine.evaluate(evaluatedDottedName)
+
+      // Then
+      expect(evaluated.nodeValue).toStrictEqual(output)
     })
-    // When
-    const engine = new Engine(rules)
-    engine.setSituation(situation)
-    const evaluated = engine.evaluate(evaluatedDottedName)
-
-    // Then
-    expect(evaluated.nodeValue).toEqual(output)
   })
 }
 
-function sum(a, b) {
-  return a + b
-}
-
-export { testOf, sum }
+export { testOf }
