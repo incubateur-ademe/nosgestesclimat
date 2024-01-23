a8ed6d4dbfb8755aa6b9fe8e822119ff6108c4f4
Emile Rolley <emile.rolley@tuta.io>
2024-01-23 10:59:21 +0100

    nitpicks(scripts): clean the generated indexes and add the nosgestesclimat.model.json in the package

--- prepack.mjs
+++ prepack.mjs
@@ -11,13 +11,18 @@ const originModelFile = 'co2-model.FR-lang.fr.json'
 const destPath = 'nosgestesclimat.model.json'
 
 const everyModelPath = path.join(process.cwd(), everyModelFolder)
-const everyModel = fs.readdirSync(everyModelPath).filter(file => file.startsWith('co2'))
+const everyModel = fs
+  .readdirSync(everyModelPath)
+  .filter((file) => file.startsWith('co2'))
 
 // Copying the origin model file to the root of the package (for use in others publicodes projects)
-fs.copyFileSync(path.join(process.cwd(), `${everyModelFolder}/${originModelFile}`), destPath)
+fs.copyFileSync(
+  path.join(process.cwd(), `${everyModelFolder}/${originModelFile}`),
+  destPath
+)
 
 // Generating index and types for each model
-everyModel.forEach(model => {
+everyModel.forEach((model) => {
   const splitName = model.split('-')
 
   const regionCode = splitName[1].split('.')[1]
@@ -26,52 +31,45 @@ everyModel.forEach(model => {
 
   const destPath = `versions/${regionCode}/${locale}${isOptim ? '/optim' : ''}`
 
-  fs.mkdirSync(destPath, { recursive: true });
+  fs.mkdirSync(destPath, { recursive: true })
 
   fs.writeFileSync(
     destPath + '/index.js',
-    generateIndex(`../../${isOptim ? '../': ''}${everyModelFolder}/${model}`)
+    generateIndex(`../../${isOptim ? '../' : ''}${everyModelFolder}/${model}`)
   )
-  fs.writeFileSync(destPath+'/index.d.ts', generateTypes(`${everyModelFolder}/${model}`))
+  fs.writeFileSync(
+    destPath + '/index.d.ts',
+    generateTypes(`${everyModelFolder}/${model}`)
+  )
   console.log(`✅ index and types generated for ${regionCode} - ${locale}`)
 })
 
 // Generating main index and types
-fs.writeFileSync(
-  'index.js',
-  generateIndex(`./${destPath}`)
-)
+fs.writeFileSync('index.js', generateIndex(`./${destPath}`))
 
 fs.writeFileSync('index.d.ts', generateTypes(destPath))
 console.log(`✅ main index and types generated`)
 
-
 function generateIndex(pathToModel) {
-  return `
-import rules from "${pathToModel}" assert { type: "json" };
+  return `import rules from "${pathToModel}" assert { type: "json" };
       
-export default rules;
-  `
+export default rules;`
 }
 
 function generateTypes(destPath) {
-  const rawData = fs.readFileSync(destPath);
-  const model = JSON.parse(rawData);
+  const rawData = fs.readFileSync(destPath)
+  const model = JSON.parse(rawData)
+
+  const dFile = `import { Rule } from "publicodes";
   
-  let indexDTypes = Object.keys(model).reduce(
-    (acc, dottedName) => acc + `| "${dottedName}"\n`,
-    `
-  import { Rule } from "publicodes";
+export type DottedName =
+${Object.keys(model)
+  .map((dottedName) => `  | "${dottedName}"`)
+  .join('\n')}
+
+declare let rules: Record<DottedName, Rule>
   
-  export type DottedName = 
-  `
-  )
-  
-  indexDTypes += `
-  declare let rules: Record<DottedName, Rule>
-  
-  export default rules;
-  `
-  
-  return indexDTypes
+export default rules;`
+
+  return dFile
 }
