55fdf9376d409c50e03960c8e752afa4e521ac16
Emile Rolley <emile.rolley@tuta.io>
2024-02-12 16:51:17 +0100

    nitpick: remove forgotten test file

--- test.ts
+++
@@ -1,9 +0,0 @@
-import base from './public/co2-model.FR-lang.fr.json'
-import opti from './public/co2-model.FR-lang.fr-opti.json'
-import Engine from 'publicodes'
-
-const baseEngine = new Engine(base)
-const optiEngine = new Engine(opti, { allowOrphanRules: true })
-
-console.log(baseEngine.evaluate('alimentation . déchets . composter').nodeValue)
-console.log(optiEngine.evaluate('alimentation . déchets . composter').nodeValue)
