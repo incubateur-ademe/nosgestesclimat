4f45805142d682792a9ef24130f20110bb71ec49
Clément <clem.auger@hotmail.fr>
2024-05-08 13:15:25 +0200

    fix: format file

--- scripts/convertSituation/convertPersonasTo2Tonnes.mjs
+++ scripts/convertSituation/convertPersonasTo2Tonnes.mjs
@@ -2,12 +2,17 @@ import personas from '@incubateur-ademe/nosgestesclimat/public/personas-fr.json'
 import convertTo2Tonnes from './convertTo2Tonnes.mjs'
 import path from 'path'
 import fs from 'fs'
+import { format, resolveConfig } from 'prettier'
 
 Object.entries(personas).map(([_, persona]) => {
   persona['surveyVariables'] = convertTo2Tonnes(persona.situation)
 })
 
-fs.writeFileSync(
-  path.join('./2Tonnes', 'personas.json'),
-  JSON.stringify(personas)
-)
+resolveConfig(process.cwd()).then((prettierConfig) => {
+  format(JSON.stringify(personas), {
+    ...prettierConfig,
+    parser: 'json'
+  }).then((formattedContent) => {
+    fs.writeFileSync(path.join('./2Tonnes', 'personas.json'), formattedContent)
+  })
+})
