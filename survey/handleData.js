2141167585aacb939762f910c1ff4db82cca6371
Chloe M <chloe.molle@neuf.fr>
2024-05-30 14:55:41 +0200

    end of NGE script

--- survey/handleData.js
+++ survey/handleData.js
@@ -1,28 +1,32 @@
 import rules from '../public/co2-model.FR-lang.fr.json' with { type: "json" }
 import Engine from 'publicodes'
 import { handleInputExcel, initOutputExcel } from './helpers/utils.js';
-import { getSituationForTransport } from './helpers/transport.js';
-import personas from "../public/personas-fr.json" with { type: "json" };
 
 const { inputworksheet, numberOfRow } = await handleInputExcel();
 const { outputWorkbook, outputWorksheet, headerRow } = await initOutputExcel();
-const engine = new Engine(rules);
 
-// for (let i = 3; i <= numberOfRow; i ++) {
-//     const currentRow = inputworksheet.getRow(i);
+for (let i = 2; i <= numberOfRow; i ++) {
+    const engine = new Engine(rules);
+    const headerInput = inputworksheet.getRow(1).values;
+    const currentRow = inputworksheet.getRow(i).values;
+    const situation = {};
+
+    for (let headerIndex = 2; headerIndex <= headerInput.length; headerIndex ++) {
+        if (headerInput[headerIndex] !== undefined && currentRow[headerIndex] !== "" && currentRow[headerIndex] !== undefined) {
+            situation[headerInput[headerIndex]] = currentRow[headerIndex];
+        }
+    }
+    engine.setSituation(situation);
     
-    engine.setSituation(personas["personas . yoram"].situation);
-    
-    const computeRow = ["yoram"];
+    const computeRow = [currentRow[1]];
 
     for (let ruleIndex = 2; ruleIndex <= headerRow.length; ruleIndex ++) {
         const rule = headerRow[ruleIndex];
-        console.log(rule);
         if (rule) computeRow.push(engine.evaluate(rule).nodeValue);
         else computeRow.push(0);
     }
 
     outputWorksheet.addRow(computeRow);
-// }
+}
 
 await outputWorkbook.xlsx.writeFile("survey/test-output.xlsx");
