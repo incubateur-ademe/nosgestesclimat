90a2f4aa5dfccd141cc518d0fff3fa50bb39007c
Clément <clem.auger@hotmail.fr>
2024-07-29 14:46:48 +0200

    fix: valeurs par défaut 2T directement intégrées

--- scripts/convertSituation/convertTo2Tonnes.mjs
+++ scripts/convertSituation/convertTo2Tonnes.mjs
@@ -10,6 +10,19 @@ export default function convertTo2Tonnes(personaSituation) {
   )
   const conversionRules = utils.readYAML(conversionRulesFile)
 
+  const default2TFile = path.join(
+    process.cwd(),
+    'scripts/convertSituation/2Tonnes/default2T.yaml'
+  )
+
+  const default2T = utils.readYAML(default2TFile)
+
+  const ifAbsent2TFile = path.join(
+    process.cwd(),
+    'scripts/convertSituation/2Tonnes/ifAbsent2T.yaml'
+  )
+  const ifAbsent2T = utils.readYAML(ifAbsent2TFile)
+
   const rules = { ...NGCRules, ...conversionRules }
   const engine = new Engine(rules)
 
@@ -27,26 +40,27 @@ export default function convertTo2Tonnes(personaSituation) {
     const formattedKey = key.replace(/-/g, '_')
 
     switch (nodeValue) {
+      case 'default':
+        NGCSituationAs2T[formattedKey] = default2T[formattedKey]
+        break
+      case 'absent':
+        NGCSituationAs2T[formattedKey] = ifAbsent2T[formattedKey]
+        break
       case 'NO':
         NGCSituationAs2T[formattedKey] = 'NO'
+        break
+      case false:
+        NGCSituationAs2T[formattedKey] = 'FALSE'
         break
       case 'YES':
         NGCSituationAs2T[formattedKey] = 'YES'
         break
       case true:
-        NGCSituationAs2T[formattedKey] = 'YES'
+        NGCSituationAs2T[formattedKey] = 'TRUE'
         break
-      case false: {
-        if (conversionRules[key] === 'non') {
-          NGCSituationAs2T[formattedKey] = false
-        } else {
-          NGCSituationAs2T[formattedKey] = 'NO'
-        }
-        break
-      }
       case undefined:
       case null:
-        NGCSituationAs2T[formattedKey] = 'NO'
+        NGCSituationAs2T[formattedKey] = ifAbsent2T[formattedKey]
         break
       default:
         NGCSituationAs2T[formattedKey] = nodeValue
