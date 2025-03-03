dd47c2d611b679245bbbadda564429a88a6432bc
Clément <clem.auger@hotmail.fr>
2025-03-03 17:37:00 +0100

    pkg: finally update publicodes with last released

--- bench/index.ts
+++ bench/index.ts
@@ -1,6 +1,5 @@
 import { bench, group, run } from 'mitata'
 import Engine from 'publicodes'
-import OldEngine from 'publicodes-old'
 import type { NGCRules } from '../index'
 import {
   getLocalPersonas,
@@ -8,7 +7,7 @@ import {
   getRulesFromDist
 } from '../tests/commons.mjs'
 
-const oldRules = await getRulesFromDist('nightly', 'FR', 'fr')
+// const oldRules = await getRulesFromDist('nightly', 'FR', 'fr')
 const newRules = await getLocalRules('FR', 'fr')
 const optimRules = await getLocalRules('FR', 'fr', true)
 const localPersonas = await getLocalPersonas('FR', 'fr')
@@ -24,13 +23,13 @@ const options = {
   logger: { warn: () => {}, error: () => {}, log: () => {} }
 }
 
-const oldEngine = new OldEngine(oldRules as unknown as NGCRules, options)
+// const oldEngine = new OldEngine(oldRules as unknown as NGCRules, options)
 const engine = new Engine(newRules as unknown as NGCRules, options)
 
 group('Parsing initial des règles', () => {
-  bench('all rules with old engine', () => {
-    new OldEngine(oldRules as unknown as NGCRules, options)
-  })
+  // bench('all rules with old engine', () => {
+  //   new OldEngine(oldRules as unknown as NGCRules, options)
+  // })
   bench('all rules with new engine', () => {
     new Engine(newRules as unknown as NGCRules, options)
   })
@@ -44,10 +43,10 @@ group('Parsing initial des règles', () => {
 })
 
 group('Evaluation', () => {
-  bench('bilan with old engine', () => {
-    oldEngine.setSituation({})
-    oldEngine.evaluate('bilan')
-  })
+  // bench('bilan with old engine', () => {
+  //   oldEngine.setSituation({})
+  //   oldEngine.evaluate('bilan')
+  // })
   bench('bilan with new engine', () => {
     engine.setSituation({})
     engine.evaluate('bilan')
@@ -56,9 +55,9 @@ group('Evaluation', () => {
 
 group('setSituation', () => {
   const situation = getRandomPersonaSituation()
-  bench('setSituation with old engine', () => {
-    oldEngine.setSituation(situation)
-  })
+  // bench('setSituation with old engine', () => {
+  //   oldEngine.setSituation(situation)
+  // })
   bench('setSituation with new engine', () => {
     engine.setSituation(situation)
   })
