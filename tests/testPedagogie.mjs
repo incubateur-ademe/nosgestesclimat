704d5b07b3d092b253fd58761c9e9b284f145cc0
Clément <clem.auger@hotmail.fr>
2025-09-30 11:33:45 +0200

    feat: meilleure couverture des actions pedagogie

---
+++ tests/testPedagogie.mjs
@@ -0,0 +1,61 @@
+import { getArgs, getLocalRules, printResults } from './commons.mjs'
+
+/**
+ * Check pédagogie.publicodes file to ensure that all actions are still valid
+ */
+
+const { country, language, markdown } = getArgs()
+
+const baseRules = await getLocalRules(country, language)
+
+const actionRules = baseRules['actions']?.formule?.somme
+
+const pedagogieActionRules = Object.keys(baseRules).reduce((acc, rule) => {
+  if (rule.startsWith('ui . pédagogie . sous catégories')) {
+    const actions = baseRules[rule]?.actions || []
+    return acc.concat(actions)
+  }
+  return acc
+}, [])
+
+const missingActions = []
+const invalidActions = []
+
+for (const action of pedagogieActionRules) {
+  if (!actionRules.includes(action)) {
+    invalidActions.push(action)
+  }
+}
+
+for (const action of actionRules) {
+  if (!pedagogieActionRules.includes(action)) {
+    missingActions.push(action)
+  }
+}
+
+console.log(`[ Test pédagogie actions ]\n`)
+
+if (missingActions.length === 0 && invalidActions.length === 0) {
+  console.log('All actions are valid!')
+  process.exit(0)
+} else {
+  console.log('Missing actions in pédagogie.publicodes:')
+  if (missingActions.length > 0) {
+    for (const action of missingActions) {
+      console.log(` - ${action}`)
+    }
+    console.log()
+  } else {
+    console.log(' - None\n')
+  }
+  console.log('Invalid actions in pédagogie.publicodes:')
+  if (invalidActions.length > 0) {
+    for (const action of invalidActions) {
+      console.log(` - ${action}`)
+    }
+    console.log()
+  } else {
+    console.log(' - None\n')
+  }
+  process.exit(0)
+}
