746834bcda15b41d07a76bb20841a45ecccba4d8
Clément <clem.auger@hotmail.fr>
2026-04-21 14:29:04 +0200

    fix: gh pages doc workflow

--- quick-doc/src/App.tsx
+++ quick-doc/src/App.tsx
@@ -33,7 +33,7 @@ const router = createBrowserRouter([
     ),
     children: [
       {
-        path: '/',
+        index: true,
         element: (
           <RouteWrapper>
             <HomePage />
